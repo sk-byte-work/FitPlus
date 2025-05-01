49fb0f0704e4784025c8c3a1efe302a3a02a3827
senthilkaasi.rsv <senthilkaasi.rsv@gmail.com>
2025-05-01 18:50:57 +0530

    Project Starter

---
+++ src/main/java/com/example/fitplus/FitplusApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitplusApplication.class, args);
+	}
+
+}
